d1c0f855c4184ae06bfdb1d4eac6058100772f4f
kaushikpaul <kaushik.paul755@gmail.com>
2022-05-28 19:27:59 +0530

    Add kafka config in application.yml and Generate Kafka topic

--- library-events-producer/src/main/java/com/kafka/producer/controller/LibraryEventsController.java
+++ library-events-producer/src/main/java/com/kafka/producer/controller/LibraryEventsController.java
@@ -1,7 +1,9 @@
 package com.kafka.producer.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.kafka.producer.domain.LibraryEvent;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class LibraryEventsController {
@@ -9,5 +11,12 @@ public class LibraryEventsController {
     @GetMapping("/ping")
     public String test_ping() {
         return "Producer is Active !!!";
+    }
+
+    @PostMapping("/v1/libraryevent")
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) {
+
+        // initialize kafka producer
+        return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 }
