a8cf456aafa7d3cf1d9a51a3f64fb87524c465a9
kaushikpaul <kaushik.paul755@gmail.com>
2022-05-29 13:24:03 +0530

    Add kafka producer

--- library-events-producer/src/main/java/com/kafka/producer/controller/LibraryEventsController.java
+++ library-events-producer/src/main/java/com/kafka/producer/controller/LibraryEventsController.java
@@ -1,6 +1,9 @@
 package com.kafka.producer.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.kafka.producer.domain.LibraryEvent;
+import com.kafka.producer.producer.LibraryEventProducer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,15 +11,20 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class LibraryEventsController {
 
+    @Autowired
+    private LibraryEventProducer libraryEventProducer;
+
     @GetMapping("/ping")
     public String test_ping() {
         return "Producer is Active !!!";
     }
 
     @PostMapping("/v1/libraryevent")
-    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) {
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
 
         // initialize kafka producer
+        libraryEventProducer.sendLibraryEvents(libraryEvent);
+
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 }
