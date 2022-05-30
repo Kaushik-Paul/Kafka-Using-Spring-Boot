3953fbcbad31a9caa3be507e13505e1be32f2975
kaushikpaul <kaushik.paul755@gmail.com>
2022-05-30 11:00:35 +0530

    Add approach 3 of kafka producer creation using Producer Record

--- library-events-producer/src/main/java/com/kafka/producer/controller/LibraryEventsController.java
+++ library-events-producer/src/main/java/com/kafka/producer/controller/LibraryEventsController.java
@@ -2,6 +2,7 @@ package com.kafka.producer.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.kafka.producer.domain.LibraryEvent;
+import com.kafka.producer.domain.LibraryEventType;
 import com.kafka.producer.producer.LibraryEventProducer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,7 +25,8 @@ public class LibraryEventsController {
 
     @PostMapping("/v1/libraryevent")
     public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
-
+        // set the library event
+        libraryEvent.setLibraryEventType(LibraryEventType.NEW);
         // initialize kafka producer
         libraryEventProducer.sendLibraryEvents(libraryEvent);
 
@@ -33,10 +35,23 @@ public class LibraryEventsController {
 
     @PostMapping("/v1/libraryevent-syn")
     public ResponseEntity<LibraryEvent> postLibraryEventSynchronous(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException {
+        // set the library event
+        libraryEvent.setLibraryEventType(LibraryEventType.NEW);
 
         // initialize kafka producer synchronously
         SendResult<Integer, String> sendResult = libraryEventProducer.sendLibraryEventsSynchronous(libraryEvent);
 
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+    }
+
+    @PostMapping("/v1/libraryevent-producer")
+    public ResponseEntity<LibraryEvent> postLibraryEventProducerRecord(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException {
+
+        // set the library event
+        libraryEvent.setLibraryEventType(LibraryEventType.NEW);
+        // initialize kafka producer synchronously
+        libraryEventProducer.sendLibraryEventsUsingProducerRecord(libraryEvent);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
--- library-events-producer/src/main/java/com/kafka/producer/producer/LibraryEventProducer.java
+++ library-events-producer/src/main/java/com/kafka/producer/producer/LibraryEventProducer.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kafka.producer.domain.LibraryEvent;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.Header;
+import org.apache.kafka.common.header.internals.RecordHeader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
@@ -11,6 +14,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 @Component
@@ -22,6 +26,8 @@ public class LibraryEventProducer {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    private String topic = "library-events";
 
     public void sendLibraryEvents(LibraryEvent libraryEvent) throws JsonProcessingException {
 
@@ -54,6 +60,33 @@ public class LibraryEventProducer {
             throw e;
         }
         return sendResult;
+    }
+
+    public void sendLibraryEventsUsingProducerRecord(LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException {
+        Integer key = libraryEvent.getLibraryEventId();
+        String value = objectMapper.writeValueAsString(libraryEvent);
+
+        ProducerRecord<Integer, String> producerRecord = buildProducerRecord(key, value, topic);
+        ListenableFuture<SendResult<Integer, String>> listenableFuture = kafkaTemplate.send(producerRecord);
+        listenableFuture.addCallback(new ListenableFutureCallback<SendResult<Integer, String>>() {
+            @Override
+            public void onFailure(Throwable ex) {
+                handleFailure(key, value, ex);
+            }
+
+            @Override
+            public void onSuccess(SendResult<Integer, String> result) {
+                handleSuccess(key, value, result);
+            }
+        });
+    }
+
+    private ProducerRecord<Integer, String> buildProducerRecord(Integer key, String value, String topic) {
+        // without record header
+//        return new ProducerRecord<>(topic, key, value);
+        // with record header
+        List<Header> recordHeader = List.of(new RecordHeader("event-source", "scanner".getBytes()));
+        return new ProducerRecord<>(topic, null, key, value, recordHeader);
     }
 
     private void handleSuccess(Integer key, String value, SendResult<Integer, String> result) {
